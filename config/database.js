152bffe6c81bde6f6b95ac3e8cb9d4cfcf9a95c6
wecraft-agency <WeC2021!raft>
2021-09-02 11:05:21 +0200

    'hi'

--- config/database.js
+++ config/database.js
@@ -1,20 +1,21 @@
- module.exports = ({ env }) => ({
-   defaultConnection: 'default',
-   connections: {
-     default: {
-       connector: 'mongoose',
-       settings: {
-         host: env('DATABASE_HOST'),
-         srv: env.bool('DATABASE_SRV', true),
-         port: env.int('DATABASE_PORT', 27017),
-         database: env('DATABASE_NAME'),
-         username: env('DATABASE_USERNAME'),
-         password: env('DATABASE_PASSWORD'),
-       },
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        host: env('DATABASE_HOST', 'cluster0.o5vyq.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'strapi-test'),
+        username: env('DATABASE_USERNAME', 'strapi-test'),
+        password: env('DATABASE_PASSWORD', 'Havgimig123.'),
+      },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-      ssl: env.bool('DATABASE_SSL', true),
+        ssl: env.bool('DATABASE_SSL', true),
+      },
     },
   },
-  },
 });
+
