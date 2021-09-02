d80d88a0564cfffe41cccba817ca8aaf4132ccb1
wecraft-agency <WeC2021!raft>
2021-09-02 10:55:05 +0200

    added conten type

--- config/plugins.js
+++ config/plugins.js
@@ -1,14 +1,20 @@
- module.exports = ({ env }) => ({
-  upload: {
-    provider: 'cloudinary',
-       providerOptions: {
-           cloud_name: env('CLOUDINARY_NAME'),
-           api_key: env('CLOUDINARY_KEY'),
-           api_secret: env('CLOUDINARY_SECRET'),
-         },
-        actionOptions: {
-            upload: {},
-            delete: {},
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        host: env('DATABASE_HOST', 'cluster0.o5vyq.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'strapi-test'),
+        username: env('DATABASE_USERNAME', 'strapi-test'),
+        password: env('DATABASE_PASSWORD', 'Havgimig123.'),
+      },
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
+      },
+    },
   },
-   },
 });
